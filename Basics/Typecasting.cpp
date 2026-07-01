5b4f5232b8dd81f6795a96b729cb0ca9ff4bfd6d
ChiragPandav67 <pandavchirag67@gmail.com>
2026-07-01 17:43:46 +0530

    Create Typecasting.cpp

---
+++ Basics/Typecasting.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //char x = 'A';
+    //int ascii = (int)x; //Explicit Typecasting
+    //cout<<ascii;
+    
+    // char x = 'a';
+    // int ascii = x;  //Implicit Typecasting
+    // cout<<ascii;
+    
+    // int x = 74;
+    // cout<<(char)x;
+    
+    // char ch1 = 'A';
+    // char ch2 = 'a';
+    // cout<<ch1 + ch2<<endl;
+    
+    cout<<char('A' + 1);
+    
+}

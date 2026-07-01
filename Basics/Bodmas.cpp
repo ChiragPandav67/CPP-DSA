e45160f2c924fd2827fcbdb7a8d397f239a032d8
ChiragPandav67 <pandavchirag67@gmail.com>
2026-07-01 17:47:56 +0530

    Update Bodmas.cpp

--- Basics/Bodmas.cpp
+++ Basics/Bodmas.cpp
@@ -3,4 +3,7 @@ using namespace std;
 int main(){
     int x = 2 / 3 * 6;
     cout<<x;
+    
+    //Multiplication and division have same precedence
+    //On same precedence, we go left to right
 }

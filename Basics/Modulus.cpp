379aa721b7a756dcb02ff939f7e862aaff0d10a2
ChiragPandav67 <pandavchirag67@gmail.com>
2026-07-01 15:49:27 +0530

    Create Modulus.cpp

---
+++ Basics/Modulus.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //cout<<25%0<<endl:  (error)
+    cout<<45 % 245<<endl;
+    cout<<34 % 10<<endl;
+    cout<<34 % (-10)<<endl;
+    cout<<(-34) % 10<<endl;
+    cout<<(-34) % (-10)<<endl;
+}

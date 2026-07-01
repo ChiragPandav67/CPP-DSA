4b39b8d4e1c6a938dc71d0e3526802de01440332
ChiragPandav67 <pandavchirag67@gmail.com>
2026-07-01 17:23:00 +0530

    Create SumOfTwoNumbers.cpp

---
+++ Basics/SumOfTwoNumbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a;
+    cout<<"Enter first number: ";
+    cin>>a;
+    
+    float b;
+    cout<<"Enter second number: ";
+    cin>>b;
+    
+    float sum = a + b;
+    cout<<"Sum: "<<sum;
+}
